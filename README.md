66c901e36e1a20489e6eafe10c88cb75603b1a88
Angelina Fabbro <angelinafabbro@gmail.com>
2013-10-10 17:20:15 +0200

    Add README, add HTML template to import

---
+++ README.md
@@ -0,0 +1,19 @@
+Quick and Dirty Web Components Demos
+=====
+
+There are a number of really awesome frameworks employing polyfills for various features of web components. The demos include Shadow DOM, HTML Templates, and HTML Imports.
+
+I wanted to take a few of these features and show how they work without any framework sugar or polyfills. So far, Chrome is the only browser to implement these natively, with the exception of HTML Templates - Firefox implements the <template> tag too!
+
+Note that `createShadowRoot()` as defined in the spec is prefixed as `webkitCreateShadowRoot()` in Chrome's implementation. Also, you will need to visit `chrome://flags` to enable HTML Imports, as it is still an experimental feature.
+
+Why no sugar and no polyfills? A lot of people keep asking me for minimal examples of how this stuff works. There are some excellent examples at HTML5 Rocks, but here is a repo you can fork and play with directly.
+
+I hacked this together in the aftermath of a very terrible bout of sickness, so pull requests to correct any shortcomings are encouraged and appreciated.
+
+- A
+
+License
+=====
+
+MIT.
--- examples.js
+++ examples.js
@@ -13,6 +13,20 @@ window.addEventListener("load", function ()
         event.preventDefault();
         remixCats();
     }, false);
+
+    document.getElementById("append-cats").addEventListener("submit", function (event)
+    {
+    	// Hijack form submission to play with things
+        event.preventDefault();
+        appendCats();
+    }, false);
+
+    document.getElementById("import-cats").addEventListener("submit", function (event)
+    {
+    	// Hijack form submission to play with things
+        event.preventDefault();
+        appendCatsFromImport();
+    }, false);
 }, false);
 
 function remixCats(){
@@ -20,32 +34,71 @@ function remixCats(){
 	var best_cats = Array.prototype.slice.call(document.querySelectorAll(":checked"));
 
 	// A string to concatenate our insertion points of our best cats in an unordered list
-	var remix_cats_markup = "<ul>"
+	var remix_cats_markup = ""
 
 	// Iterate over best cats, add classname to parent, concatenate our remixed cats markup
 	best_cats.forEach(function(cat){
 		cat_class = "best-cat-" + cat.value;
 		cat.parentNode.className = cat_class;
+
+		// We use the <content> tag to tell the shadow tree to use elements from the host node
+		// and we use the 'select' attribute to pass in a class name to choose which ones
+
 		remix_cats_markup += "<content select=\"." + cat_class + "\"></content>"
 	});
-
-	// Close the unordered list of best cats
-	remix_cats_markup += "</ul>"
-
-	console.log(remix_cats_markup)
 	
 	// Create a host node
-	// var host = document.createElement('div');
 
 	var host = document.getElementById('cats');
 
 	// Create a shadow root
-	var shadow = host.createShadowRoot();
+	var shadow = host.webkitCreateShadowRoot();
 
 	// Set the innerHTML of our shadow root, thus causing the replacement of example-1's rendering
 	// to be that of our shadow root using our remixed cats markup string
 
 	shadow.innerHTML = remix_cats_markup;
-	//document.body.appendChild(host);
 
+}
+
+function appendCats(){
+	// A div to host an instance of our cats template
+	var el = document.createElement('div');
+
+	// The element that we will append each instance of the cats template to
+
+	var future_cats = document.getElementById('future-cats');
+
+	// Create a shadow root
+	var shadow = el.webkitCreateShadowRoot();
+
+	// We append the content of our template to the shadow root
+	shadow.appendChild(document.getElementById('cats-template').content);
+
+	// We append our newly formed div with template content to the DOM
+	future_cats.appendChild(el);
+
+	// TODO: Figure out why when we append more than one div of shadow-cats, we don't see multiples despite their existence in the DOM
+	// Probably I'm missing something obvious here in my post-sick fugue or something
+}
+
+function appendCatsFromImport(){
+	// A div to host an instance of our cats template
+	var el = document.createElement('div');
+
+	// The element that we will append each instance of the cats template to
+
+	var imported_cats = document.getElementById('imported-cats');
+
+	// Create a shadow root
+	var shadow = el.webkitCreateShadowRoot();
+
+	// We append the content of our template to the shadow root
+	shadow.appendChild(document.getElementById('import-cats-template').content);
+
+	// We append our newly formed div with template content to the DOM
+	imported_cats.appendChild(el);
+
+	// TODO: Figure out why when we append more than one div of shadow-cats, we don't see multiples despite their existence in the DOM
+	// Probably I'm missing something obvious here in my post-sick fugue or something
 }
